bc67f3df16afdd7a0cf93c3fef1295bca45c680a
Kleii203 <160496670+FredAndrei203@users.noreply.github.com>
2025-11-01 00:39:29 +0800

    Refactored tile action on occupation

--- the_gridmap/grid_tile_walkable.gd
+++ the_gridmap/grid_tile_walkable.gd
@@ -3,16 +3,11 @@ class_name GridTileWalkable extends GridTile
 enum OccupiedBy {NONE, SNAKE_PART, APPLE}
 var occupied_by: OccupiedBy = OccupiedBy.NONE
 
-func _on_area_entered(area: Area2D) -> void:
+func _action_when_entered_by(area: Area2D) -> void:
 	if area is Apple:
 		occupied_by = OccupiedBy.APPLE
 	elif area is SnakeHitbox:
 		occupied_by = OccupiedBy.SNAKE_PART
-	
-	is_occupied = true
-	%Label.text = "O"
 
-func _on_area_exited(_area: Area2D) -> void:
-	is_occupied = false
+func _action_when_exited_by(area: Area2D) -> void:
 	occupied_by = OccupiedBy.NONE
-	%Label.text = "X"
