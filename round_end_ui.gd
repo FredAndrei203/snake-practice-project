ff796819967cf9b5a5a702dd5daa5e67c80af064
Kleii203 <160496670+FredAndrei203@users.noreply.github.com>
2025-11-01 22:04:25 +0800

    Connected button signals to parent

--- round_end_ui.gd
+++ round_end_ui.gd
@@ -4,3 +4,11 @@ var message: String:
 	set(new_value):
 		message = new_value
 		%Label.text = message
+
+
+func _on_play_again_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_menu_button_pressed() -> void:
+	pass # Replace with function body.
